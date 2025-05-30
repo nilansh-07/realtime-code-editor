aeae0c7bbee2438578277182e9466028f164610d
Nilansh Kumar <111874409+nilansh-07@users.noreply.github.com>
2025-05-30 23:34:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",  
+    origin: "origin: https://realtime-code-editor-ten-mocha.vercel.app/",  
     methods: ["GET", "POST"],
     credentials: true
   },
